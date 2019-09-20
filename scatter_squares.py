1ea3003f53e0b5397bdb6965a45c9c5083dfec57
Robert Stowers <robert.stowers@gmail.com>
2019-09-20 00:05:39 -0400

    Created sqaured scatter pairs via list comprehension

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+# List of numbers from 1 to 1000 -- last number not included
+x_values = list(range(1, 1001))
+# List comprehension generates y-values
+y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, s=100)
+plt.scatter(x_values, y_values, s=40)
 
 # Set chart title and label axes.
 plt.title("Square Numbers", fontsize=24)
@@ -13,6 +15,9 @@ plt.ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
 plt.tick_params(axis='both', which='major', labelsize=14)
+
+# Set the range for each axis.
+plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
 
