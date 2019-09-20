cbad9afe09fe08b3d862b6d1a9a109a56b8dcccd
Robert Stowers <robert.stowers@gmail.com>
2019-09-20 00:13:42 -0400

    Uses color map option

--- scatter_squares.py
+++ scatter_squares.py
@@ -6,9 +6,15 @@ x_values = list(range(1, 1001))
 # List comprehension generates y-values
 y_values = [x**2 for x in x_values]
 
-# For color can use RGB color model.
-# Values closer to 0 produce darker colors; closer to 1 produce lighter colors
-plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolor='none', s=40)
+# # Can color the scatter dots
+# plt.scatter(x_values, y_values, c='red', edgecolor='none', s=40)
+
+# # For color can use RGB color model.
+# # Values closer to 0 produce darker colors; closer to 1 produce lighter colors
+# plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolor='none', s=40)
+
+# Can use a color map & how to assign how to use it
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolor='none', s=40)
 
 # Set chart title and label axes.
 plt.title("Square Numbers", fontsize=24)
