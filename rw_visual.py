b32015292be17c600e14d9b0768c3ae61a9a730a
Robert Stowers <robert.stowers@gmail.com>
2019-09-23 13:56:13 -0400

    Added points to random walk

--- rw_visual.py
+++ rw_visual.py
@@ -4,11 +4,11 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active.
 while True:
     # Make a random walk, and plot the points.
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
 
     # Emphasize the first and last points.
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
