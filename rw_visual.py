b3832ea0704c51331f04a9c255a4aaf1b52c4dcf
Robert Stowers <robert.stowers@gmail.com>
2019-09-25 13:31:47 -0400

    Updated scatter random walk to add second series

--- rw_visual.py
+++ rw_visual.py
@@ -4,19 +4,24 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active.
 while True:
     # Make a random walk, and plot the points.
-    rw = RandomWalk(50000)
+    rw = RandomWalk(10000)
     # rw = RandomWalk(5000)
     rw.fill_walk()
+
+    rw2 = RandomWalk(10000)
+    rw2.fill_walk()
 
     # Set the size of the plotting window.
     plt.figure(figsize=(10, 6))
 
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=10)
+    plt.scatter(rw2.x_values, rw2.y_values, c=point_numbers, cmap=plt.cm.Oranges, edgecolor='none', s=10)
 
     # Emphasize the first and last points.
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    plt.scatter(rw2.x_values[-1], rw2.y_values[-1], c='red', edgecolors='none', s=100)
 
     # # Line Plot
     # plt.plot(rw.x_values, rw.y_values, linewidth=1)
